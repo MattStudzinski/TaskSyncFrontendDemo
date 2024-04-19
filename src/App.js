3ce151f9c7fd8a725d6da47ff2006158c2da0e27
Matthew Studzinski <88520969+MattStudzinski@users.noreply.github.com>
2024-04-18 19:38:48 -0700

    header and routes set

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import IssueForm from './componets/forms/IssueForm';
-
+import Dashboard from './componets/dash/Dashboard';
+import Header from './componets/Header';
 
 function App() {
   return (
     <>
-    <IssueForm />
+    <BrowserRouter>
+    <Header />
+    <div className='pages'>
+      <Routes>
+        <Route
+        path="/"
+        element={<Dashboard />}
+        />
+      </Routes>
+    {/* <IssueForm /> */}
+    </div>
+    </BrowserRouter>
     </>
   );
 }
