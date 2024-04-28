b66cc6b03074b9aae1c1dd4a00088d287553d41c
Matthew Studzinski <88520969+MattStudzinski@users.noreply.github.com>
2024-04-27 20:27:07 -0700

    token created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,9 @@
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
-
-
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' } )
+}
 
 const loginUser = async (req, res) => {
     res.json({mssg: "login user"})
@@ -12,8 +14,10 @@ const signupUser = async (req, res) => {
 
     try {
         const user = await User.signup(email, password)
+        // create token
+        const token = createToken(user._id)
 
-        res.status(200).json({email, user})
+        res.status(200).json({email, token})
     } catch (error) {
         res.status(400).json({error: error.message})
     }
