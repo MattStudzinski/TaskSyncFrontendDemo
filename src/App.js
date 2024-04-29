41d5f58021e53143bab5e0c1ce61895f771035ec
Matthew Studzinski <88520969+MattStudzinski@users.noreply.github.com>
2024-04-28 23:11:48 -0700

    form functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import IssueForm from './componets/forms/IssueForm';
 import Dashboard from './componets/dash/Dashboard';
 import Header from './componets/Header';
+import LoginForm from './componets/forms/LoginForm';
+import Signup from './componets/forms/Signup';
 
 
 function App() {
@@ -10,11 +12,20 @@ function App() {
     <>
     <BrowserRouter>
     <Header />
+    
     <div className='pages'>
       <Routes>
         <Route
         path="/"
         element={<Dashboard />}
+        />
+        <Route
+        path="/login"
+        element={<LoginForm />}
+        />
+        <Route
+        path="/signup"
+        element={<Signup />}
         />
       </Routes>
     {/* <IssueForm /> */}
--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -8,6 +8,12 @@ const Header = () => {
                 <Link to="/">
                     <h1>Task Trove</h1>
                 </Link>
+                <nav>
+                    <div>
+                        <Link to="/login">Login</Link>
+                        <Link to="/signup">Signup</Link>
+                    </div>
+                </nav>
             </div>
         </header>
     );
