ba99e19149baecdc64dea4205c3b07ca05562387
Matthew Studzinski <88520969+MattStudzinski@users.noreply.github.com>
2024-05-12 23:43:22 -0700

    cards made

--- src/componets/dash/Analytics.jsx
+++ src/componets/dash/Analytics.jsx
@@ -3,16 +3,23 @@ import React from 'react';
 const Analytics = () => {
     return (
         <section className='section-analytics'>
-            <div className='row'>
-            <div className='col-1-of-3'>
-                Open Issues
+            <div className='section-analytics__box'>
+                <div >
+                    Open Issues
+                </div>
+                <p className='section-analytics__text'>hello</p>
             </div>
-            <div className='col-1-of-3'>
-                New Issues
+            <div className='section-analytics__box'>
+                <div>
+                    new issues
+                </div>
+                <p className='section-analytics__text'>hello</p>
             </div>
-            <div className='col-1-of-3'>
-            High Priority
-            </div>
+            <div className='section-analytics__box'>
+                <div>
+                    High Priority
+                </div>
+                <p className='section-analytics__text'>hello</p>
             </div>
         </section>
     );
--- src/componets/dash/Dashboard.jsx
+++ src/componets/dash/Dashboard.jsx
@@ -11,16 +11,6 @@ const Dashboard = () => {
             <Analytics />
             <DashIssues />
             <IssueForm />
-            <section className='grid-test'>
-                <div className='row'>
-                    <div className='col-1-of-2'>
-                        col-1-of-2
-                    </div>
-                    <div className='col-1-of-2'>
-                        col-1-of-2
-                    </div>
-                </div>
-            </section>
         </div>
     );
 };
