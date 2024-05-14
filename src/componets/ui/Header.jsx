1dd6eb730790e3f01b72552dfe3b0d144a6d86f2
Matthew Studzinski <88520969+MattStudzinski@users.noreply.github.com>
2024-05-13 22:51:39 -0700

    set to start css

--- src/componets/ui/Header.jsx
+++ src/componets/ui/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useLogout } from '../../hooks/useLogout';
+import add from "../../svgs/add-circle-svgrepo-com.svg"
 
 const Header = () => {
     const {logout} = useLogout()
@@ -9,22 +10,40 @@ const Header = () => {
     }
     return (
         <header className='header'>
-            <div className='header__container'>
                 <Link to="/">
-                    <h1>Task Trove</h1>
+                    <h1 className='logo'>Task Trove</h1>
                 </Link>
-                <nav>
-                    <div>
+                
+                    <form action= "#"className='search'>
+                        <input type='text' className='search__input' placeholder='Search Issues'/>
+                        <button className='search__button'>
+                        <img src={add} alt="Add" className="search__svg" />
+                        </button>
+                    </form>
+                    <nav className='user-nav'>
+
+                    <div className='user-nav__logout-box'>
                         <button onClick={handleClick}>Log out</button>
                     </div>
-                    <div>icon for account, dropdown for</div>
-                    <div>
-                        <Link to="/login">Login</Link>
-                        <Link to="/signup">Signup</Link>
-                        <Link to="/alltickets">View Tasks</Link>
+
+                    <div className='user-nav__login-box'>
+                        <Link to="/login"><img src={add} alt="Add" className="user-nav__icon" /></Link>
+                    </div>
+
+                    <div className='user-nav__signup-box'>
+                        <Link to="/signup"><img src={add} alt="Add" className="user-nav__icon" /></Link>
+                    </div>
+
+                    <div className='user-nav__alltickets-box'>
+                        <Link to="/alltickets"><img src={add} alt="Add" className="user-nav__icon" /></Link>
+                        <span className='user-nav__notification'>3</span>
+                    </div>
+
+                    <div className='user-nav__user'>
+                        <img src={add} alt='user profile' className='user-nav__user-picture'/>
+                        <span className='user-nav__username'>TEC002009740</span>
                     </div>
                 </nav>
-            </div>
         </header>
     );
 };
