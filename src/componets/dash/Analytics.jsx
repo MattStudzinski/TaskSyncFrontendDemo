87bb6d8c30987b3585c75b8b173b42132105d99d
Matthew Studzinski <88520969+MattStudzinski@users.noreply.github.com>
2024-05-30 21:27:09 -0700

    can track high priority and new issues within a day

--- src/componets/dash/Analytics.jsx
+++ src/componets/dash/Analytics.jsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useIssuesContext } from '../../hooks/useIssuesContext';
 
 const Analytics = () => {
+    const { issues } = useIssuesContext()
+    const [highPriority, setHighPriority] = useState(0)
+    const [newIssues, setNewIssues] = useState(0)
+
+
+    useEffect(() => {
+        const highPriorityIssues = issues.filter(issue => issue.priority === 'high')
+        setHighPriority(highPriorityIssues.length)
+
+        const dateTracker = new Date()
+        dateTracker.setDate(dateTracker.getDate() - 1)
+
+        const newIssues = issues.filter(issue => {
+            const issueDate = new Date(issue.createdAt)
+            return issueDate >= dateTracker
+        })
+
+        setNewIssues(newIssues.length)
+
+    }, [issues])
+
+
     return (
         <section className='analytics'>
             <div className='analytics__open-issues'>
@@ -11,13 +34,13 @@ const Analytics = () => {
             </div>
             <div className='analytics__new-issues'>
                 <div>
-                    new issues
+                    new issues {newIssues}
                 </div>
                 <p className='analytics__box-text'>hello</p>
             </div>
             <div className='analytics__priority-issues'>
                 <div>
-                    High Priority
+                    High Priority {highPriority}
                 </div>
                 <p className='analytics__box-text'>hello</p>
             </div>
