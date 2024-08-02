ace7facfc116819181bbdf87fb7f0c2efd81361a
Matthew Studzinski <88520969+MattStudzinski@users.noreply.github.com>
2024-08-01 22:03:54 -0700

    logout logic is fixed and styed button is good

--- src/componets/ui/Sidebar.jsx
+++ src/componets/ui/Sidebar.jsx
@@ -5,8 +5,21 @@ import Completed from '../../svgs/SVG/CompletedTasks'
 import Priority from '../../svgs/SVG/Priority';
 import Stats from '../../svgs/SVG/Stats';
 import AddTask from '../../svgs/SVG/AddTask';
+import { useAuthContext } from '../../hooks/useAuthContext';
+import {useNavigate} from 'react-router-dom'
 
 const Sidebar = () => {
+
+    const {dispatch} = useAuthContext()
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        localStorage.removeItem('user')
+        dispatch({ type: 'LOGOUT' })
+        navigate('/login')
+    }
+
+
     return (
         <nav className='sidebar'>
             <ul className='side-nav'>
@@ -47,6 +60,13 @@ const Sidebar = () => {
                     <span className='side-nav__link-title'>Admin</span>
                 </Link>
                 </li>
+
+                <li className='side-nav__item'>
+                    <button onClick={handleLogout} className="logout-button">
+                        <span className='side-nav__link-title'>Logout</span>
+                    </button>
+                </li>
+                
             </ul>
         </nav>
     );
