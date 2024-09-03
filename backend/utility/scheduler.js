ce3835c711e1ceb9889d372a25411e200d9fab34
Matthew Studzinski <88520969+MattStudzinski@users.noreply.github.com>
2024-09-02 22:44:47 -0700

    update path login

--- backend/utility/scheduler.js
+++
@@ -1,6 +0,0 @@
-// const cron = require('node-cron')
-// const createRecurringIssues = require('./recurringIssues')
-
-// cron.schedule('0 0 * * *', () => {
-//     createRecurringIssues()
-// })
--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -13,7 +13,7 @@ export const useLogin = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch('/api/user/login', {
+        const response = await fetch('https://tasksyncbackend-01eb2bc18fab.herokuapp.com//api/user/login', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({name, password})
